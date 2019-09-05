7c4f486f1db315a4489a1a319446a78645301ad2
Weslley Barbosa Magalhães <weslley.magalhaes@triersistema.com.br>
2019-09-05 00:57:57 -0300

    Correção do Adapter mockado

--- app/src/main/java/dev/weslley/plutapp/fragments/ExchangeStockFragmentFragment.kt
+++ app/src/main/java/dev/weslley/plutapp/fragments/ExchangeStockFragmentFragment.kt
@@ -1,18 +1,13 @@
 package dev.weslley.plutapp.fragments
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import dev.weslley.plutapp.R
 
-import dev.weslley.plutapp.fragments.dummy.DummyContent
-import dev.weslley.plutapp.fragments.dummy.DummyContent.DummyItem
+
 
 /**
  * A fragment representing a list of Items.
@@ -21,58 +16,13 @@ import dev.weslley.plutapp.fragments.dummy.DummyContent.DummyItem
  */
 class ExchangeStockFragmentFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
-
-    private var listener: OnListFragmentInteractionListener? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_exchangestockfragment_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyExchangeStockFragmentRecyclerViewAdapter(DummyContent.ITEMS, listener)
-            }
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_exchangestockfragment_list, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onListFragmentInteraction(item: DummyItem?)
-    }
 
 }
